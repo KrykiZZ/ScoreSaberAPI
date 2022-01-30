e19110fbfb4d12a8ed23adfaab326ddbf2b7185c
KrykiZZ <krykizzzz@gmail.com>
2022-01-30 04:51:11 +0300

    Fix url building when previous parameters is missing.

--- ScoreSaberAPI/ScoreSaber.cs
+++ ScoreSaberAPI/ScoreSaber.cs
@@ -3,6 +3,7 @@ using ScoreSaberAPI.Models;
 using ScoreSaberAPI.Responses;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -44,45 +45,45 @@ namespace ScoreSaberAPI
             int? maxStar = null, Category? category = null, Sort? sort = null,
             bool? unique = null, int? page = null, bool? withMetadata = true)
         {
-            string url = $"https://{_scoreSaberDomain}/api/leaderboards";
+            Dictionary<string, string> query = new Dictionary<string, string>();
 
             if (search != null)
-                url += $"?search={search}";
+                query.Add("search", search);
 
             if (verified != null)
-                url += $"&verified={verified}";
+                query.Add("verified", verified.ToString());
 
             if (ranked != null)
-                url += $"&ranked={ranked}";
+                query.Add("ranked", ranked.ToString());
 
             if (qualified != null)
-                url += $"&qualified={qualified}";
+                query.Add("qualified", qualified.ToString());
 
             if (loved != null)
-                url += $"&loved={loved}";
+                query.Add("loved", loved.ToString());
 
             if (minStar != null)
-                url += $"&minStar={minStar}";
+                query.Add("minStar", minStar.ToString());
 
             if (maxStar != null)
-                url += $"&maxStar={maxStar}";
+                query.Add("maxStar", maxStar.ToString());
 
             if (sort != null)
-                url += $"&sort={(int)sort}";
+                query.Add("sort", sort.ToString());
 
             if (category != null)
-                url += $"&category={(int)category}";
+                query.Add("category", category.ToString());
 
             if (unique != null)
-                url += $"&unique={unique}";
+                query.Add("unique", unique.ToString());
 
             if (page != null)
-                url += $"&page={page}";
+                query.Add("page", page.ToString());
 
             if (withMetadata != null)
-                url += $"&withMetadata={withMetadata}";
+                query.Add("withMetadata", withMetadata.ToString());
 
-            return await _http.GetFromJsonAsync<GetLeaderboardsResponse>(url);
+            return await _http.GetFromJsonAsync<GetLeaderboardsResponse>(BuildUrl($"https://{_scoreSaberDomain}/api/leaderboards", query));
         }
 
         public async Task<LeaderboardInfo> GetLeaderboard(int id)
@@ -92,47 +93,47 @@ namespace ScoreSaberAPI
 
         public async Task<LeaderboardInfo> GetLeaderboard(string hash, Difficulty difficulty, string gameMode = null)
         {
-            string url = $"https://{_scoreSaberDomain}/api/leaderboard/by-hash/{hash}/info?difficulty={(int)difficulty}";
+            Dictionary<string, string> query = new Dictionary<string, string>();
             if (gameMode != null)
-                url += $"&gameMode={gameMode}";
+                query.Add("gameMode", gameMode);
 
-            return await _http.GetFromJsonAsync<LeaderboardInfo>(url);
+            return await _http.GetFromJsonAsync<LeaderboardInfo>(BuildUrl($"https://{_scoreSaberDomain}/api/leaderboard/by-hash/{hash}/info?difficulty={(int)difficulty}", query));
         }
 
         public async Task<GetLeaderboardsScoresResponse> GetScores(int leaderboardId, string countries = null, string search = null, int? page = null, bool? withMetadata = null)
         {
-            string url = $"https://{_scoreSaberDomain}/api/leaderboard/by-id/{leaderboardId}/scores";
+            Dictionary<string, string> query = new Dictionary<string, string>();
             if (countries != null)
-                url += $"?countries={countries}";
+                query.Add("countries", countries);
 
             if (search != null)
-                url += $"&search={search}";
+                query.Add("search", search);
 
             if (page != null)
-                url += $"&page={page}";
+                query.Add("page", page.ToString());
 
             if (withMetadata != null)
-                url += $"&withMetadata={withMetadata}";
+                query.Add("withMetadata", withMetadata.ToString());
 
-            return await _http.GetFromJsonAsync<GetLeaderboardsScoresResponse>(url);
+            return await _http.GetFromJsonAsync<GetLeaderboardsScoresResponse>(BuildUrl($"https://{_scoreSaberDomain}/api/leaderboard/by-id/{leaderboardId}/scores", query));
         }
 
         public async Task<GetLeaderboardsScoresResponse> GetScores(string hash, Difficulty difficulty, string countries = null, string search = null, int? page = null, bool? withMetadata = null)
         {
-            string url = $"https://{_scoreSaberDomain}/api/leaderboard/by-hash/{hash}/scores?difficulty={(int)difficulty}";
+            Dictionary<string, string> query = new Dictionary<string, string>();
             if (countries != null)
-                url += $"?countries={countries}";
+                query.Add("countries", countries);
 
             if (search != null)
-                url += $"&search={search}";
+                query.Add("search", search);
 
             if (page != null)
-                url += $"&page={page}";
+                query.Add("page", page.ToString());
 
             if (withMetadata != null)
-                url += $"&withMetadata={withMetadata}";
+                query.Add("withMetadata", withMetadata.ToString());
 
-            return await _http.GetFromJsonAsync<GetLeaderboardsScoresResponse>(url);
+            return await _http.GetFromJsonAsync<GetLeaderboardsScoresResponse>(BuildUrl($"https://{_scoreSaberDomain}/api/leaderboard/by-hash/{hash}/scores?difficulty={(int)difficulty}", query));
         }
 
         public async Task<List<LeaderboardDifficulty>> GetDifficulties(string hash)
@@ -142,32 +143,32 @@ namespace ScoreSaberAPI
 
         public async Task<GetPlayersResponse> GetPlayers(string search = null, int? page = null, string countries = null, bool? withMetadata = null)
         {
-            string url = $"https://{_scoreSaberDomain}/api/players";
+            Dictionary<string, string> query = new Dictionary<string, string>();
             if (search != null)
-                url += $"?search={search}";
+                query.Add("search", search);
 
             if (page != null)
-                url += $"&page={page}";
+                query.Add("page", page.ToString());
 
             if (countries != null)
-                url += $"&countries={countries}";
+                query.Add("countries", countries);
 
             if (withMetadata != null)
-                url += $"&withMetadata={withMetadata}";
+                query.Add("withMetadata", withMetadata.ToString());
 
-            return await _http.GetFromJsonAsync<GetPlayersResponse>(url);
+            return await _http.GetFromJsonAsync<GetPlayersResponse>(BuildUrl($"https://{_scoreSaberDomain}/api/players", query));
         }
 
         public async Task<int> GetPlayersCount(string search = null, string countries = null)
         {
-            string url = $"https://{_scoreSaberDomain}/api/players/count";
+            Dictionary<string, string> query = new Dictionary<string, string>();
             if (search != null)
-                url += $"?search={search}";
+                query.Add("search", search);
 
             if (countries != null)
-                url += $"&countries={countries}";
+                query.Add("countries", countries);
 
-            return Convert.ToInt32(await _http.GetStringAsync(url));
+            return Convert.ToInt32(await _http.GetStringAsync(BuildUrl($"https://{_scoreSaberDomain}/api/players/count", query)));
         }
 
         public async Task<Player> GetPlayerBasic(long? playerId)
@@ -182,20 +183,28 @@ namespace ScoreSaberAPI
 
         public async Task<GetPlayerScoresResponse> GetPlayerScores(long? playerId, int? limit = null, ScoresSort? sort = null, int? page = null, bool? withMetadata = null)
         {
-            string url = $"https://{_scoreSaberDomain}/api/player/{playerId}/scores";
+            Dictionary<string, string> query = new Dictionary<string, string>();
+
             if (limit != null)
-                url += $"?limit={limit}";
+                query.Add("limit", limit.ToString());
 
             if (sort != null)
-                url += $"&sort={sort.ToString().ToLower()}";
+                query.Add("sort", sort.ToString().ToLower());
 
             if (page != null)
-                url += $"&page={page}";
+                query.Add("page", page.ToString());
 
             if (withMetadata != null)
-                url += $"&withMetadata={withMetadata}";
+                query.Add("withMetadata", withMetadata.ToString());
 
-            return await _http.GetFromJsonAsync<GetPlayerScoresResponse>(url);
+            return await _http.GetFromJsonAsync<GetPlayerScoresResponse>(BuildUrl($"https://{_scoreSaberDomain}/api/player/{playerId}/scores", query));
+        }
+
+        public string BuildUrl(string url, Dictionary<string, string> query)
+        {
+            if (query.Count > 0)
+                return $"{url}?{string.Concat(query.Select(x => $"&{x.Key}={x.Value}")).Remove(0, 1)}";
+            return url;
         }
     }
 }
